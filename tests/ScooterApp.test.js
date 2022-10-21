c3a26931f76b6e9a52eb120d8bc941f2420a5ee6
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-10-21 12:55:37 +0100

    Created ScooterApp classes that tests registering users and logging in users

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,12 +1,25 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
-const ScooterApp = require('../src/ScooterApp')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
+const ScooterApp = require("../src/ScooterApp");
 
 // ScooterApp tests here
+describe("ScooterApp Tests", () => {
+  test("Registering user", () => {
+    const user = new User("diogoluz", "1234", "18");
 
-// register user
+    const scooterApp = new ScooterApp();
+    scooterApp.register(user);
+  });
 
-// log in
+  test("Logging in user", () => {
+    const user = new User("diogoluz", "1234", "18");
+
+    const scooterApp = new ScooterApp();
+    scooterApp.register(user);
+    console.log(scooterApp);
+    scooterApp.logIn("diogoluz", "1234");
+  });
+});
 
 // add scooter
 
