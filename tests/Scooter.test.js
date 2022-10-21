7088ea1033d2be643024cc8c1450c6fc129c2109
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-10-21 13:08:59 +0100

    Written test to see if scooter is equal to an object

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,19 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe("scooter object", () => {
+  test("Checks if scooter is an object", () => {
+    const scooter = new Scooter("Manhattan", "diogoluz");
+    expect(typeof scooter).toBe("object");
+  });
+});
 
 //Method tests
-describe('scooter methods', () => {
+describe("scooter methods", () => {
   // tests here!
-
   //rent method
-
   //dock method
-
   //requestRepair method
-
   //charge method
-
-})
+});
