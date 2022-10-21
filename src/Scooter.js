f1f4193237831faedd4b0f8b12996404eb2f156b
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-10-21 12:54:46 +0100

    Creating Scooter Class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,37 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  #user;
+  constructor(station, user) {
+    this._station = station;
+    this.#user = user;
+    this._serial = Math.floor(Math.random() * 1000);
+    this._charge = Math.floor(Math.random() * 1000);
+    this._isBroken = false;
+    this._docked = true;
+  }
+
+  get station() {
+    return this._station;
+  }
+
+  get user() {
+    return this.#user;
+  }
+
+  get serial() {
+    return this._serial;
+  }
+
+  get charge() {
+    return this._charge;
+  }
+
+  get isBroken() {
+    return this._isBroken;
+  }
+
+  get docked() {
+    return this._docked;
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
