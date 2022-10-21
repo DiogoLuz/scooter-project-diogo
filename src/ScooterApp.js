8e4671dfe5b522b12c0f6ba20dfc2cad37672ad2
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-10-21 12:54:58 +0100

    Creating Scooter App Class

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,8 +1,56 @@
-const User = require('./User')
-const Scooter = require('./Scooter')
+const User = require("./User");
+const Scooter = require("./Scooter");
 
 class ScooterApp {
-  // ScooterApp code here
+  static scooterSessions = [];
+
+  constructor() {
+    this._stations = {
+      Manhattan: [],
+      Brooklyn: [],
+      Queens: [],
+      Bronx: [],
+      Statenisland: [],
+    };
+
+    this._registeredUsers = {};
+
+    ScooterApp.scooterSessions.push(this);
+  }
+
+  get registeredUsers() {
+    return this._registeredUsers;
+  }
+
+  register(user) {
+    if (this._registeredUsers[user.name] === user.name) {
+      console.log("already registered!");
+    } else {
+      if (user.age > 17) {
+        this._registeredUsers[user.name] = {
+          password: user.password,
+          age: user.age,
+          loggedIn: false,
+          accountChange: 0,
+        };
+
+        console.log("user has been registered");
+      }
+    }
+  }
+
+  logIn(username, password) {
+    console.log(this._registeredUsers[username]);
+    if (
+      username in this._registeredUsers &&
+      this._registeredUsers[username].password === password
+    ) {
+      this._registeredUsers[username].loggedIn = true;
+      console.log("user has been logged in");
+    } else {
+      throw new Error("Username and password is not registered.");
+    }
+  }
 }
 
-module.exports = ScooterApp
+module.exports = ScooterApp;
